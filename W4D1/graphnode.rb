1e8fda45814a2931189e7d9b38373fedf1e1ffdb
claudiaaziz <claudiahannahh@gmail.com>
2023-09-11 22:23:24 -0400

    not finished

--- W4D1/graphnode.rb
+++ W4D1/graphnode.rb
@@ -1,3 +1,4 @@
+# require the set
 require 'set'
 
 class GraphNode
@@ -9,28 +10,39 @@ class GraphNode
     end
 
     def self.bfs(starting_node, target_value)
+        # start the queue with the starting node
         queue = [starting_node]
+        # start a new set
         visited = Set.new
+        # add the first node to visited
         visited.add(queue[0])
 
+        # keep looping until the queue is empty
         until queue.empty?
+            # get the current node
             current_node = queue.shift
 
+            # if currentnode.value is == to target value return bc found target
             return current_node if current_node.value == target_value
 
+            # loop through current nodes neighbors
             current_node.neighbors.each do |next_node|
+                # if the next node has not been visited
                 if !visited.include?(next_node)
+                    # add it to the queue
                     queue << next_node
+                    # put it in visitedd
                     visited.add(next_node)
                 end
             end
         end
 
+        # if no nodes value is found return nil
         nil
     end
 end
 
-a = GraphNode.new('a')
+a = GraphNode.new('a ')
 b = GraphNode.new('b')
 c = GraphNode.new('c')
 d = GraphNode.new('d')
---
+++ practice-for-ch-ruby-mancala-main/lib/board.rb
@@ -0,0 +1,62 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @cups = Array.new(14) { [] }
+    @cups.each_index do |i|
+      @cups[i] += [:stone, :stone, :stone, :stone] if i != 6 && i != 13
+    end
+  end
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
+  end
+
+  def valid_move?(start_pos)
+    raise 'Invalid starting cup' if start_pos > @cups.length
+    raise 'Starting cup is empty' if @cups[start_pos].empty?
+  end
+
+  def make_move(start_pos, current_player_name)
+    distribute = @cups[start_pos]
+    @cups[start_pos] = []
+
+    until distribute.empty?
+      (start_pos...@cups.length).each do |next_cup_i|
+        if @cups[next_cup_i].empty?
+          return :switch
+        elsif @cups[next_cup_i] == current_player_name.side
+          return :prompt
+        else
+          return next_cup_i
+        end
+        @cups[next_cup_i] << :stone
+        distribute.pop
+        render
+      end
+    end
+
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine what #make_move returns
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+    side_1 = @cups[0...6]
+    side_2 = @cups[7...12]
+    side_1.all? { |cup| cup.empty? } || side_2.all? { |cup| cup.empty? }
+  end
+
+  def winner
+    
+  end
+end
