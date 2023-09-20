722cda5bd029196f28018a486b1d71729cd95d21
claudiaaziz <claudiahannahh@gmail.com>
2023-09-20 06:30:42 -0400

    not finished

--- W5D3/practice-for-ch-sql-plays/plays.rb
+++ W5D3/practice-for-ch-sql-plays/plays.rb
@@ -15,11 +15,11 @@ class Play
   attr_accessor :id, :title, :year, :playwright_id
 
   def self.find_by_title(title)
-
+    data.execute("SELECT title FROM plays WHERE title = title")
   end
 
   def self.find_by_playwright(name)
-
+    data.execute("SELECT name FROM plays WHERE name = name")
   end
 
   def self.all
@@ -60,19 +60,30 @@ end
 
 class Playwright
   def self.all
-
+    data = PlayDBConnection.instance.execute("SELECT * FROM plays")
+    data.map { |datum| Playwright.new(datum) }
   end
 
   def self.find_by_name(name)
-
+    data.execute("SELECT name FROM plays WHERE name = name")
   end
 
-  def initialize
-
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @year = options['year']
+    @play_id = options['play_id']
   end
 
   def create
-
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.title, self.year, self.play_id)
+      INSERT INTO
+        playwright (title, year, play_id)
+      VALUES
+        (?, ?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
   end
 
   def update
